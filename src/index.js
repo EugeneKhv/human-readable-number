8b8a889e7421e5f3c3a2cddedb27c4b1eb316273
Evgeniy Khvoynov <53225787+EugeneKhv@users.noreply.github.com>
2022-06-28 00:38:51 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 module.exports = function toReadable (number) {
-   let numArr = [["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"], ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"], ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]];
+let numArr = [["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"], ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"], ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]];
     if (number >= 0 && number <= 99) {
         return dd(number);
     }
     if (number >= 99 && number <= 999) {
         let n = +number.toString()[0];
-        return dd(n) + ' hundred ' + dd(+number.toString()[1]+ number.toString()[2]);
+        if (+number.toString()[2] != 0 && +number.toString()[1] != 0) {
+            return dd(n) + ' hundred ' + dd(number.toString()[1] + number.toString()[2]);
+        }
+        if ((+number.toString()[1] == 0)) {
+            return dd(n) + ' hundred ' + dd(number.toString()[2]);
+        }
+        if ((+number.toString()[2] == 0)) {
+            return dd(n) + ' hundred ' + dd(number.toString()[1] + number.toString()[2]).replace(/zero/gi, '');
+        }
     }
     function dd(num) {
         if (num >= 0 && num <= 9) {
@@ -27,7 +35,7 @@ module.exports = function toReadable (number) {
             return numArr[2][3] + " " + numArr[0][num.toString()[1]];
         }
         if (num >= 60 && num <= 69) {
-            return numArr[2][4] +  " " + numArr[0][num.toString()[1]];
+            return numArr[2][4] + " " + numArr[0][num.toString()[1]];
         }
         if (num >= 70 && num <= 79) {
             return numArr[2][5] + " " + numArr[0][num.toString()[1]];
